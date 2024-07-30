ea044d1372bf3084b49ae18bea8867ba26e31e97
aleMontoya22 <vladioso2020@gmail.com>
2024-07-29 18:52:22 -0600

    Añadi paginas, donutchart, linechart y piechart con su funcionalidad y el paquete react-router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import BarChart from './Charts/BarChart';
+import DoughnutChart from './Charts/DonutChart';
+import PieChart from './Charts/PieChart';
+import LineChart from './Charts/LineChart';
 
 function App() {
   return (
    <div className='App'>
-    <BarChart/>
+    <h1>Gráficas de criptomonedas</h1>
+    <DoughnutChart/>
    </div>
   );
 }
